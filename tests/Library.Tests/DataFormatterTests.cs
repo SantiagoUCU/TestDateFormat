87f0e6bbd019708a60f41af4a77e44e823703d3e
santiago <santi.iribarnegaray@correo.ucu.edu.uy>
2022-09-07 20:11:04 -0300

    git

---
+++ tests/Library.Tests/DataFormatterTests.cs
@@ -0,0 +1,16 @@
+namespace Library.Tests;
+
+[TestFixture]
+public class DateFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+    [Test]
+    public void Test1()
+    {
+        
+    }
+}
